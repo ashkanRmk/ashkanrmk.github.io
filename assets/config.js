e14f1ec99d48a235dec9b5e0b3257221d2eba1ba
Ashkan Rahmani <ashkan.rmk@hotmail.com>
2017-06-15 00:26:11 +0430

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,65 @@
+
+window.cpanel = {
+    data: {
+        phone: "",
+        fax: "",
+        email: "mail@ashkanam.ir",
+        address: "",
+        social: [
+            
+            
+            {
+                icon: 'twitter',
+                link: "https:\/\/twitter.com\/ashkan_rmk"
+            },
+            
+            
+            {
+                icon: 'instagram',
+                link: "https:\/\/instagram.com\/ashkan.rmk"
+            },
+            
+            
+            
+            {
+                icon: 'linkedin',
+                link: "https:\/\/ir.linkedin.com\/in\/ashkanrmk"
+            },
+            
+                        {
+                icon: 'telegram',
+                link: "https://t.me/Ashkan_rmk"
+            },
+            
+                                {
+                icon: 'video',
+                link: "http://www.aparat.com/AshkanRahmani_GYM"
+            },
+
+                        
+                                {
+                icon: 'git',
+                link: "https://github.com/ashkan-rmk"
+            },
+        ]
+    },
+    style: {
+        primary: "",
+        accent: ""
+    },
+    slides: [
+        {
+            prefix: "I'm",
+            title: "Ashkan Rahmani",
+            subtitle: "Developer , Designer and Fitness Instructor",
+            type: 'vcard',
+            backgroundImage: "",
+            backgroundColor: "",
+            color: "",
+            buttonText: "Visit My Blog",
+            buttonLink: "http:\/\/ashkan-rmk.ir\/blog\/",
+            biography: "I'm an IT engineering student currently living in Mashhad, Iran. My interests range is from Graphic Designing to Gymnastics. I am also interested in Android development, Programming, and Education.\n\nYou can click the button below to visti my blog. I'll be happy if you share with me your ideas and feedback. If you’d like to get in touch, feel free to say hello through any of the social links below." ,
+            portraitImage: ""
+        }
+    ]
+};
